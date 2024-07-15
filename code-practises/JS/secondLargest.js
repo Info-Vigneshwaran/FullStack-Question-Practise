d0ceda5cfb53870eb8e9ee74b9f93c9e1369c42d
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2024-07-15 17:46:35 +0530

    set 6

--- code-practises/JS/secondLargest.js
+++ code-practises/JS/secondLargest.js
@@ -16,19 +16,44 @@
 // const alphaArr = arr.filter((val)=> typeof val === "string")
 // console.log([...numArr,...alphaArr]);
 
+// let matrix = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+// ];
+// const rotateArr = matrix.map((value,index)=>{
+//     return value.map((_val,index2)=>matrix[matrix.length - 1 -index2][index])
+// })
 
-let matrix = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-];
-const rotateArr = matrix.map((value,index)=>{
-    return value.map((val,index2)=>matrix[matrix.length - 1 -index2][index])
+// const amsNum = 15;
+// const amsNumber = amsNum.toString().split("").reduce((acc,val)=>{
+//     return acc += Math.pow(val, 3)
+// },0)
+
+// console.log(amsNumber===amsNum);
+
+// const vowels = ['a','e','i','o','u']
+// const inputStr = "aagggsdfkjkwewqeqwoioiosiocsocdoio";
+
+// const vowelsArr = inputStr.split("").reduce((acc,val)=>{
+//     if(vowels.includes(val) ) { acc[val] = (acc[val] || 0) + 1}
+//     return acc
+// },{})
+// console.log(vowelsArr);
+
+// const curryFn = (a) => (b) => (c) => {return a+b+c}
+// const param1 = curryFn(2)
+// const param2 = param1(2)(5)
+// console.log(param2);
+const input6 = [10,9,8,7,6]
+const myFunction = ((arr,index)=>{
+    return arr[index]
 })
+console.log(myFunction([10,9,8,7,6],5))
 
-const amsNum = 15;
-const amsNumber = amsNum.toString().split("").reduce((acc,val)=>{
-    return acc += Math.pow(val, 3)
-},0)
-
-console.log(amsNumber===amsNum);
+const myFunction2 = ((arr,position)=>{
+    return arr.splice(arr.length - position,
+        position);
+    
+})
+console.log(myFunction2(input6,2))
