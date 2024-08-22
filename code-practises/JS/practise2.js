4e085512637e00e9f51759da12d5216b39c3777a
Info-Vigneshwaran <info.vigneshwaran@gmail.com>
2024-08-22 11:09:50 +0530

    practise

---
+++ code-practises/JS/practise2.js
@@ -0,0 +1,100 @@
+const data =[
+    {
+        "key":"sample 1",
+        "data":"Data 1"
+    },
+    {
+        "key":"sample 1",
+        "data":"Data 1"
+    },
+    {
+        "key":"sample 1",
+        "data":"Data 1"
+    },
+    {
+        "key":"sample 1",
+        "data":"Data 1"
+    },
+    {
+        "key":"sample 2",
+        "data":"Data 2"
+    },
+    {
+        "key":"sample 2",
+        "data":"Data 2"
+    },
+    {
+        "key":"sample 2",
+        "data":"Data 2"
+    }
+    ,
+    {
+        "key":"sample 3",
+        "data":"Data 3"
+    },
+    {
+        "key":"sample 4",
+        "data":"Data 4"
+    },
+    {
+        "key":"sample 5",
+        "data":"Data 5"
+    }
+]
+const uniqArr = {}
+
+// const result = Object.groupBy(data, ({ key }) => key);
+// console.log(result)
+const findMultiple = (({data})=>{
+    data.forEach(item => {
+        if(uniqArr[item.key]){
+            uniqArr[item.key].push(item)
+        } else {
+            uniqArr[item.key] = [item]
+        }
+    });
+
+})
+
+// findMultiple({data})
+
+let index = 0
+const findMultipleRecursive = (({data})=>{
+   
+    if(index < data.length) {
+        if(uniqArr[data[index].key]){
+            uniqArr[data[index].key].push(data[index])
+        } else {
+            uniqArr[data[index].key] = [data[index]]
+        }
+        ++index
+        findMultipleRecursive({data})
+    }
+})
+
+findMultipleRecursive({data})
+// console.log(uniqArr);
+
+const inputStr = "PROGRAM"
+let newArr =[]
+const printSubStr = (({str1})=>{
+    const midLength = Math.floor(str1.length /2)
+    const str2 = str1.slice(midLength,str1.length)
+    let newStr = '';
+    [...str2].forEach(element => {
+        newArr = [...newArr,newStr += element]
+    });
+    [...str1].slice(0,midLength).forEach(element => {
+        newArr = [...newArr,newStr += element]
+    });
+    // for (let index = 0; index < str2.length; index++) {
+    //     newArr = [...newArr,newStr += str2[index]]
+    // }
+    // for (let index = 0; index < midLength; index++) {
+    //     newArr = [...newArr,newStr += str1[index]]
+    // }
+
+})
+
+printSubStr({str1:inputStr});
+console.log(newArr);
